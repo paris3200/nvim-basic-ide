3445a18a67c4940c58f20c90758574d1ddd7bc9f
Jason Paris <paris3200@gmail.com>
2022-07-08 17:35:44 -0400

    Add python and htop terminals

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -37,3 +37,14 @@ local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
 function _LAZYGIT_TOGGLE()
   lazygit:toggle()
 end
+local htop = Terminal:new({ cmd = "htop", hidden = true })
+
+function _HTOP_TOGGLE()
+	htop:toggle()
+end
+
+local python = Terminal:new({ cmd = "python", hidden = true })
+
+function _PYTHON_TOGGLE()
+	python:toggle()
+end
