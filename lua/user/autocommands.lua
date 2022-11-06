fa75ba23d1bb59e129343fd6efb02d2d43c69316
Jason Paris <paris3200@gmail.com>
2022-11-06 06:54:33 -0500

    Add autocommand pencil

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -13,6 +13,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
+    vim.opt_local.textwidth= 74
   end,
 })
 -- Automatically close tab/vim when nvim-tree is the last window in the tab
@@ -66,4 +67,10 @@ vim.cmd [[
   augroup _ledger
     autocmd BufWritePre *.ldg :LedgerAlignBuffer
   augroup end
+  
+  augroup pencil
+    autocmd!
+    autocmd FileType markdown,mkd call pencil#init()
+    autocmd FileType text         call pencil#init()
+  augroup END
 ]]
